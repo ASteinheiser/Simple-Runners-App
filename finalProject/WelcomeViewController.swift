99f8c2abe3ed6f832ee0031db273e499d15f5d78
ASteinheiser <asteinhe@asu.edu>
2016-11-16 14:39:13 -0700

    final version

---
+++ finalProject/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeViewController.swift
+//  finalProject
+//
+//  Created by Andrew Steinheiser on 11/16/16.
+//  Copyright © 2016 AndrewSteinheiser. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
